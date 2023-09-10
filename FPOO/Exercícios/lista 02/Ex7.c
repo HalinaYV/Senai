03af9190d0ea714e5e403ab015bb4697e58370a6
Halina <halinayelena@gmail.com>
2023-09-10 16:24:49 -0300

    Create Ex7.c

---
+++ FPOO/Exercícios/lista 02/Ex7.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+   //7 Escreva um programa que leia 5 números inteiros em qualquer ordem e exiba na tela ao final, os cinco números em ordem crescente.
+printf("Leia 5 numeros inteiros em qualquer ordem e depois os coloque em ordem crescente");
+int a, b, b, c, d;
+scanf("%d %d %d %d %d", &a, &b, &c, &c, &d);
+
+if (a != b){
+
+if ( b > a)
+}
+if (b != c){
+
+if ( b > c)
+}
+
+if (c != d){
+
+if ( c > d)
+}
+
+    printf("ordem crescente: \n");
+else
+
+return 0;
+}

97ce68610eadb2ee98f2da161fde8a28677ba951
Halina <halinayelena@gmail.com>
2023-09-10 16:15:30 -0300

    Create Ex4.c

---
+++ FPOO/Exercícios/lista 01/Ex4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main (){
+    printf("O time los Tigres, na temporada passada, teve um total de 9 vitorias e 4 empates em seus jogos. sabendo que vitorias contam 3 pontos e empates 1, temos: \n");
+    printf("Time = los Tigres \n");
+    printf("Vitorias = 8 \n");
+    printf("Empates = 4 \n");
+    int x = 8;
+    int y = 4;
+    int z = (8*3)+4*1;
+    printf("A soma total dos pontos (8*3)+4*1 = %d",z);
+    return 0;
+}

d74c88a3d0f654635a31c7a1e1086cecc0b2b62f
Halina <halinayelena@gmail.com>
2023-09-10 16:14:48 -0300

    Create Ex2.c

---
+++ FPOO/Exercícios/lista 01/Ex2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (){
+    printf("Um carro percorre 60km/h numa distancia de duas cidades proximas, sendo ela de 18km em media \n");
+    printf("para saber o tempo gasto em media, calcula-se: \n");
+    int x = 60;
+    int y = 18;
+    int z = 60;
+    int k = 60 * 18 / 60;
+    printf("sendo as grandezas inversamente proporcionais, a multiplicacao 60 * 18 / 60 = %d \n",k);
+    return 0;
+}

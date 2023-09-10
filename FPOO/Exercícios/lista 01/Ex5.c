ce077c4cdd250a75d883e95772b37ea9b77ffb14
Halina <halinayelena@gmail.com>
2023-09-10 16:15:45 -0300

    Create Ex5.c

---
+++ FPOO/Exercícios/lista 01/Ex5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (){
+    printf("Os numeros 6 e 3 serao representados no calculo a seguir deste enunciado um tanto quanto ardiloso \n");
+    int x = 6;
+    int y = 3;
+    int z = 6 + 1;
+    int k = 3 - 1;
+    printf("Exemplo 1 igual a 6 + 1 = %d \n",z);
+    printf("Exemplo 2 igual a 3 - 1 = %d \n",k);
+    return 0;
+}

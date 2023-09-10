5e394fa307f91e4f6853e3d457a6f12309b049a0
Halina <halinayelena@gmail.com>
2023-09-10 16:16:31 -0300

    Create Ex8.c

---
+++ FPOO/Exercícios/lista 01/Ex8.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (){
+    printf("Um cilindro de 15cm de altura e 3cm de raio precisa ter sua area e volume calculados, vemos a seguir \n");
+    int x = 15;
+    int y = 3;
+    int z = 2*3*9+2*3*3*15;
+    printf("A area do cilindro 2*3*9+2*3*3*15 = %d",z);
+    int k = 3*9*15;
+    printf("O volume do cilindro 3*9*15 = %d",k);
+    return 0;
+}
